6baea97fd3c7f86959cbcad793ec4b3ca445772b
Amir <amir.moravej@gmail.com>
2013-08-04 14:01:25 -0400

    register, logout and http functions

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,77 @@
+
+var hostname = 'http://localhost:8080';
+
 var login = function () {
 	var u = document.getElementById('l-u'),
-		p = document.getElementById('l-p');
+		p = document.getElementById('l-p'),
+		path = hostname + '/login';
+
+	http({
+		'method': 'POST',
+		'body': {
+			'username': u.value,
+			'password': p.value
+		},
+		'path': path,
+		callback: function(res) {
+			console.log(res);
+		}
+	});
+
+	// console.log(u.value);
+	// console.log(p.value);
 
 	return false;
 };
 
 var register = function(){
+	var u = document.getElementById('l-u'),
+		p = document.getElementById('l-p'),
+		path = hostname + '/register';
 
+	http({
+		'method': 'POST',
+		'body': {
+			'username': u.value,
+			'password': p.value
+		},
+		'path': path,
+		callback: function(res) {
+			console.log(res);
+		}
+	});
+};
+
+var logout = function(){
+	var u = document.getElementById('l-u'),
+		p = document.getElementById('l-p'),
+		path = hostname + '/logout';
+
+	http({
+		'method': 'POST',
+		'body': {
+			'username': u.value,
+			'password': p.value
+		},
+		'path': path,
+		callback: function(res) {
+			console.log(res);
+		}
+	});
+};
+
+var http = function(options){
+
+	var request = new XMLHttpRequest();
+
+	request.open(options.method, options.path);
+
+	for (var header in options.headers)
+		request.setRequestHeader(header, options.headers[header]);
+
+	request.onload = function() { 
+		if (options.callback) options.callback(eval(request.response));
+	};
+
+	request.send(options.body);
 };
