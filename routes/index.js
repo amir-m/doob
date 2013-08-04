02270e012463fd590353d16c5b60cc97d5fbc5e6
Amir <amir.moravej@gmail.com>
2013-08-04 13:00:34 -0400

    register handler

--- routes/index.js
+++ routes/index.js
@@ -44,9 +44,41 @@ module.exports = function(fs, redisClient, models){
 		});
 	}; 
 
+	var register = function(req, res){
+		if (req.session && req.session.uid) delete req.session.uid;
+
+		if (!req.body.username || req.body.username.length < 1 || 
+			!req.body.password || req.body.password.length < 1 || 
+			!req.body.name || req.body.name.length < 1 || req.body.notExists == false) {
+			console.log('POST /register: Bad Registration Request.'.error);
+			return res.send(400);
+		};
+		models.User.userExists(req.body.username, function(yes){
+			if (yes) {
+				console.log('POST /register: User Exists.'.error);
+				return res.send(400)
+			};
+	 		models.User.createUser({
+	 			username: req.body.username,
+	 			password: req.body.password,
+	 			name: req.body.name
+	 		}, function(r){
+				if (!r || (r && r.error)) {
+					console.log('models.User.createUser callback error:'.error);
+					console.log(r.error);
+					return res.send(500);
+				};
+				req.session.uid = r.id;
+				return res.send(200);
+			});
+		});
+	};
+
+
 	return {
 		index: index,
 		public: public,
-		login: login
+		login: login,
+		register: register
 	}
 };
