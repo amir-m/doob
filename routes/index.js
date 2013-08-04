87afa70f96111a12e56a13d43049846d003c0bbb
Amir <amir.moravej@gmail.com>
2013-08-04 19:26:25 -0400

    adding/removing username to/from the redisClient

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-module.exports = function(fs, redisClient, models){
+module.exports = function(fs, redis, redisClient, models){
 
 	var index = function(req, res, next){
 		if (req.session && !req.session.uid)
@@ -16,7 +16,7 @@ module.exports = function(fs, redisClient, models){
 		// return res.send('LOGIN Request Recieved for: ' + req.body.username + ' Password: ' +
 		// 	req.body.password + ' ...Thanks for loging in...');
 
-		if (req.session && req.session.uid) return res.redirect('/');
+		// if (req.session && req.session.uid) return res.redirect('/');
 
 		if (!req.body.username || req.body.username.length < 1 || 
 			!req.body.password || req.body.password.length < 1) {
@@ -30,10 +30,13 @@ module.exports = function(fs, redisClient, models){
 			if (r && r.success) {
 				console.log('POST /login Successfull Login.'.info);
 				req.session.uid = r.id;
-				// return res.send(200);
+				redisClient.set(r.id, req.body.username.toString(), redis.print);
+				redisClient.get(r.id, function(err, reply) {
+					console.log('Successfully login: ' + reply);	
+				});
 				return res.redirect('/');
 			}
-
+			
 			if (r && r.erro && r.error.code == 500) {
 				console.log('POST /login Failed to fetch the user info!'.error);
 				res.status(500);
@@ -49,10 +52,13 @@ module.exports = function(fs, redisClient, models){
 	}; 
 
 	var logout = function(req, res, next) {
-		if (! req.session || ! req.session.uid) return res.send('No user to logout bro !!!');
+		if (!req.session || !req.session.uid) return res.send('No user to logout bro !!!');
 		console.log('GET /logout, uid: ' + req.session.uid);
-		delete req.session.uid;
-		res.send('logged out');
+		redisClient.get(req.session.uid function(err, reply){
+			delete req.session.uid;
+			redisClient.del(req.session.uid, redis.print);
+			res.send('logged out: ' + reply);
+		});
 	};
 
 	var register = function(req, res, next){
@@ -82,7 +88,8 @@ module.exports = function(fs, redisClient, models){
 					return res.send(500);
 				};
 				req.session.uid = r.id;
-				console.log(req.session.uid);
+				redisClient.set(r.id, req.body.username, redis.print);
+				console.log('Successfully registered: ' + req.session.uid);
 				return res.redirect('/');
 			});
 		});
