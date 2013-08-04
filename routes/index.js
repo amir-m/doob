52ce3c6afbcbd81e6d4a57c50ecc1ab6428ac392
Amir <amir.moravej@gmail.com>
2013-08-04 00:38:16 -0400

    login handler

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 module.exports = function(fs, redisClient){
 
 	var index = function(req, res, next){
-		if (!req.uid)
+		if (req.session && !req.session.uid)
 			return res.sendfile('views/login.html');
 		else
 			return res.sendfile('views/index.html');
@@ -11,8 +11,13 @@ module.exports = function(fs, redisClient){
 		res.sendfile('public/'+req.params[0]);
 	};
 
+	var login = function(req, res, next) {
+		
+	}; 
+
 	return {
 		index: index,
-		public: public
+		public: public,
+		login: login
 	}
 };
