57e42cad64e509afd3cb5018ce3a187b6d0ae23b
Amir <amir.moravej@gmail.com>
2013-08-04 21:41:34 -0400

    register function

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,7 +3,6 @@ function hmCtrl ($scope, $http) {
 	var hostname = 'http://localhost:8080';
 
 	$scope.login = function () {
-		var path = hostname + '/login';
 
 		$http({
 			method: 'POST',
@@ -14,25 +13,26 @@ function hmCtrl ($scope, $http) {
 			headers: {
 				'Content-Type': 'application/json'
 			},
-			url: path
+			url: hostname + '/login'
 		}).success(function(res) {
 				console.log(res);
 		});
 	};
 
-	var http = function(options){
-
-		var request = new XMLHttpRequest();
-
-		request.open(options.method, options.path);
-
-		for (var header in options.headers)
-			request.setRequestHeader(header, options.headers[header]);
-
-		request.onload = function() { 
-			if (options.callback) options.callback(request.response);
-		};
-
-		request.send(JSON.stringify(options.body));
-	};
+	$scope.register = function(){
+		
+		$http({
+			method: 'POST',
+			data: {
+				'username': $scope.ru,
+				'password': $scope.rp
+			},
+			url: hostname + '/register',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).success(function(res) {
+				console.log(res);
+		});
+	}
 }
