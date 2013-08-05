fbe3b31fc68d874217692542799fa28632d16cd0
Amir <amir.moravej@gmail.com>
2013-08-05 01:21:52 -0400

    adding separate controllers

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,45 +1,45 @@
-function hmCtrl ($scope, $http) {
+function LoginCtrl = function () {
+
+	$http({
+		method: 'POST',
+		data: {
+			'username': $scope.lu,
+			'password': $scope.lp
+		},
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		url: '/login'
+	}).success(function(res) {
+			console.log(res);
+	});
+};
+
+function RegisterCtrl = function(){
 	
-	var hostname = 'http://localhost:8080';
+	$http({
+		method: 'POST',
+		data: {
+			'username': $scope.ru,
+			'password': $scope.rp
+		},
+		url: '/register',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}).success(function(res) {
+			console.log(res);
+	});
+}
 
-	$scope.login = function () {
-
-		$http({
-			method: 'POST',
-			data: {
-				'username': $scope.lu,
-				'password': $scope.lp
-			},
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			url: hostname + '/login'
-		}).success(function(res) {
-				console.log(res);
-		});
-	};
-
-	$scope.register = function(){
-		
-		$http({
-			method: 'POST',
-			data: {
-				'username': $scope.ru,
-				'password': $scope.rp
-			},
-			url: hostname + '/register',
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		}).success(function(res) {
-				console.log(res);
-		});
-	}
+function hmCtrl ($scope, $http) {
 
 	$scope.logout = function(){
 		
-		$http.get(hostname + '/logout').success(function(res) {
+		$http.get('/logout').success(function(res) {
 			console.log(res);	
 		});
 	}
-}
+};
+
+hmCtrl.$inject = ['$scope', '$http'];
