7bc50102eb25bc60a28a47d21addbb2330049e47
Amir <amir.moravej@gmail.com>
2013-08-05 15:40:40 -0400

    typo

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,28 @@
-angular.module('hm', []).
-  config(['$routeProvider', function($routeProvider) {
+angular.module('hm', []).factory('Auth', ['$http', '$location', function($http, $location){
+	return {
+		login: function(u, p){
+			$http({
+				method: 'POST',
+				data: {
+					'username': u,
+					'password': p
+				},
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				url: '/login'
+			}).success(function(res) {
+				console.log(res);
+				$location.path('/home')
+			});
+		}//,
+		// register: function(){},
+		// logout: function(){}
+	};
+}]).config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/login', {templateUrl: 'partials/login.html',   controller: LoginCtrl}).
-      when('/home', {templateUrl: 'partials/index.html',   controller: HomeCtrl}).
+      when('/login', {templateUrl: 'partials/login.html', controller: LoginCtrl}).
+      when('/home', {templateUrl: 'partials/index.html', controller: HomeCtrl}).
       // when('/register', {templateUrl: 'views/register.html', controller: RegisterCtrl}).
       otherwise({redirectTo: '/home'});
 }]);
