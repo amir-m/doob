888bc34b215abd07322d4159c3464e0e156f4b21
Amir <amir.moravej@gmail.com>
2013-08-04 20:57:03 -0400

    adding login function

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,2 +1,24 @@
 function hmCtrl ($scope, $http) {
+	var login = function () {
+		var u = document.getElementById('l-u'),
+			p = document.getElementById('l-p'),
+			path = hostname + '/login';
+
+		http({
+			'method': 'POST',
+			'body': {
+				'username': u.value,
+				'password': p.value
+			},
+			'path': path,
+			'headers': {
+				'Content-Type': 'application/json'
+			},
+			callback: function(res) {
+				console.log(res);
+			}
+		});
+
+		return false;
+	};
 }
