402eb6caafc1a2a866bbf47da6ad6b3b9d04d43d
Amir <amir.moravej@gmail.com>
2013-08-05 15:47:12 -0400

    using Auth service

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,4 +1,4 @@
-function LoginCtrl($scope, $http, $location, Auth) {
+function LoginCtrl($scope, Auth) {
 
 	$scope.lrm = true;
 	$scope.rrm = true;
@@ -6,45 +6,12 @@ function LoginCtrl($scope, $http, $location, Auth) {
 	$scope.login = function(){
 		Auth.login($scope.lu, $scope.lp);
 	};
-
-	// $scope.login = function(){
-	// 	$http({
-	// 		method: 'POST',
-	// 		data: {
-	// 			'username': $scope.lu,
-	// 			'password': $scope.lp
-	// 		},
-	// 		headers: {
-	// 			'Content-Type': 'application/json'
-	// 		},
-	// 		url: '/login'
-	// 	}).success(function(res) {
-	// 			console.log(res);
-	// 			$location.path('/home')
-	// 	});
-	// };
-
-	// Todo: set cookie for remember me...
-
 	$scope.register = function(){
-		$http({
-			method: 'POST',
-			data: {
-				'username': $scope.ru,
-				'password': $scope.rp
-			},
-			url: '/register',
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		}).success(function(res) {
-				console.log(res);
-				$location.path('/home')
-		});
+		Auth.register($scope.ru, $scope.rp);
 	};
 };
 
-function HomeCtrl ($scope, $http, $location) {
+function HomeCtrl ($scope, $http, $location, Auth) {
 
 	$http.get('/me').success(function(data, status){
 		if (status == 404) return $location.path('/login');
@@ -52,14 +19,8 @@ function HomeCtrl ($scope, $http, $location) {
 	});
 
 	$scope.logout = function(){
-		
-		$http.get('/logout').success(function(res) {
-			console.log(res);	
-			$scope.username = null;
-			$location.path('/login')
-
-			// TODO: 	redirect to login
-		});
+		$scope.username = null;
+		Auth.logout();
 	}
 };
 
