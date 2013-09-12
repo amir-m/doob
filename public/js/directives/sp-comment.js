d18ca142f2fbc8db489e4c728a5148455d2c1e65
Amir <amir.moravej@gmail.com>
2013-09-12 18:52:03 -0400

    next round-16

---
+++ public/js/directives/sp-comment.js
@@ -0,0 +1,40 @@
+define(['directives/directives'], function(directives){
+
+	directives.directive('spComment', ['doobio', '$rootScope', 'socket',
+		function(doobio, $rootScope, socket){
+	
+		return {
+				
+			link: function(scope, element, att) {
+				element.bind('keyup', function(e){
+					if (e.which == 13) {
+						// console.log('comment');
+						// console.log($(element).val());
+						if ($(element).val().length > 0)
+							socket.emit('new:soundPattern:comment', {
+								event: 'new:soundPattern:comment',
+								broadcaster: $rootScope.username,
+								commenter: $rootScope.username,
+								patternId: scope.patternInfo._id,
+								timestamp: new Date().getTime(),
+								comment: $(element).val()
+							});
+						if (scope.patternInfo && !scope.patternInfo.comments)
+							scope.patternInfo.comments = [];
+
+						scope.patternInfo.comments.push({
+							commenter: $rootScope.username,
+							patternId: scope.patternInfo._id,
+							timestamp: new Date().getTime(),
+							comment: $(element).val()
+						});
+						
+						$(element).val('');
+						$(element).blur();
+					}
+				});
+			}
+
+		}
+	}]);
+});
